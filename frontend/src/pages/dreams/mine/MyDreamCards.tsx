8fbf416d8684c16cf79932d8dca0024e8deab2b5
shunsei <k.shunsei1227@gmail.com>
2025-03-05 02:27:25 +0900

    Refactor my dream cards component

--- frontend/src/pages/dreams/mine/MyDreamCards.tsx
+++ frontend/src/pages/dreams/mine/MyDreamCards.tsx
@@ -1,5 +1,5 @@
 import { Dream } from "@/types/dream";
-import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { AlertDialog, Flex } from "@radix-ui/themes";
 import {
   FacebookIcon,
   FacebookShareButton,
@@ -32,74 +32,70 @@ const getPinkGradientClass = (likes: number): string => {
 
 const MyDreamCards = ({ myDreams, setMyDreams }: MyDreamCardsProps) => {
   return (
-    <>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-8">
-        {myDreams.map((dream, index) => (
-          <AlertDialog.Root key={index}>
-            <div className="relative">
-              <AlertDialog.Trigger>
-                <div
-                  // prettier-ignore
-                  className={`
-                    border rounded-xl shadow-lg p-4 min-h-48 py-6 bg-gradient-to-b from-white
-                    ${getPinkGradientClass(dream.likes)}
-                  `}
-                >
-                  <Flex justify="between" direction="column" align="center">
-                    <div className="mt-8 text-gray-800 overflow-hidden line-clamp-2">
-                      {dream.content}
-                    </div>
-                    <div className="font-medium mt-2 text-base pt-5 text-center">
-                      {dream.likes} いいね
-                    </div>
-                  </Flex>
-                </div>
-              </AlertDialog.Trigger>
-              <div className="absolute top-5 left-5 z-10">
-                <MyDreamPrivacyButton dream={dream} setMyDreams={setMyDreams} />
-              </div>
-              <div className="absolute top-5 right-5 z-10">
-                <MyDreamDeleteButton dream={dream} setMyDreams={setMyDreams} />
-              </div>
-            </div>
-            <AlertDialog.Content className="flex flex-col min-h-[300px] max-w-[400px]">
-              <AlertDialog.Title>{dream.title}</AlertDialog.Title>
-              <div className="mb-auto">
-                <AlertDialog.Description size="4">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-8">
+      {myDreams.map((dream, index) => (
+        <AlertDialog.Root key={index}>
+          <div className="relative transition-transform transform hover:scale-105">
+            <AlertDialog.Trigger>
+              <div
+                // prettier-ignore
+                className={`
+                  border rounded-xl shadow-lg p-5 min-h-48 bg-gradient-to-b from-white
+                  flex flex-col justify-between
+                  ${getPinkGradientClass(dream.likes)}
+                `}
+              >
+                <div className="mt-8 text-gray-800 overflow-hidden line-clamp-2">
                   {dream.content}
-                </AlertDialog.Description>
+                </div>
+                <div className="font-medium mt-5 mb-2 text-base text-center">
+                  {dream.likes} いいね
+                </div>
               </div>
-              <Flex gap="3" justify="center" direction="column" align="center">
-                <p className="text-center font-bold mt-3">
-                  ＼発信して夢を叶えよう！／
-                </p>
-                <Flex gap="2" justify="center">
-                  <FacebookShareButton
-                    url={window.location.href}
-                    title={dream.content}
-                    hashtag="#AprilDream"
-                  >
-                    <FacebookIcon size={40} round />
-                  </FacebookShareButton>
-                  <TwitterShareButton
-                    url={window.location.href}
-                    title={dream.content}
-                    hashtags={["AprilDream"]}
-                  >
-                    <XIcon size={40} round />
-                  </TwitterShareButton>
-                </Flex>
-                <AlertDialog.Cancel>
-                  <Button variant="soft" color="gray">
-                    閉じる
-                  </Button>
-                </AlertDialog.Cancel>
+            </AlertDialog.Trigger>
+            <div className="absolute top-5 left-5 z-10">
+              <MyDreamPrivacyButton dream={dream} setMyDreams={setMyDreams} />
+            </div>
+            <div className="absolute top-5 right-5 z-10">
+              <MyDreamDeleteButton dream={dream} setMyDreams={setMyDreams} />
+            </div>
+          </div>
+
+          <AlertDialog.Content className="flex flex-col h-full min-h-[300px] max-w-[400px]">
+            <AlertDialog.Title></AlertDialog.Title>
+            <AlertDialog.Description size="4" className="flex-grow">
+              {dream.content}
+            </AlertDialog.Description>
+            <Flex gap="3" direction="column" justify="end" align="center">
+              <p className="text-center font-bold mt-3">
+                ＼発信して夢を叶えよう！／
+              </p>
+              <Flex gap="2" justify="center">
+                <FacebookShareButton
+                  url={window.location.href}
+                  title={dream.content}
+                  hashtag="#AprilDream"
+                >
+                  <FacebookIcon size={40} round />
+                </FacebookShareButton>
+                <TwitterShareButton
+                  url={window.location.href}
+                  title={dream.content}
+                  hashtags={["AprilDream"]}
+                >
+                  <XIcon size={40} round />
+                </TwitterShareButton>
               </Flex>
-            </AlertDialog.Content>
-          </AlertDialog.Root>
-        ))}
-      </div>
-    </>
+              <AlertDialog.Cancel>
+                <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-5 rounded max-w-[100px]">
+                  閉じる
+                </button>
+              </AlertDialog.Cancel>
+            </Flex>
+          </AlertDialog.Content>
+        </AlertDialog.Root>
+      ))}
+    </div>
   );
 };
 
