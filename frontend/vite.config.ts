6c60ca3e1bc49ad7f7b42d1b53ea3598dbf0f6ef
cercil0605 <kuamaichi2223@gmail.com>
2025-03-05 17:30:56 +0900

    add: dockerfile & vite setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0', // 全てのIPからアクセスを許可
+    port: 5173, // 必要に応じて変更
+  }
 });
