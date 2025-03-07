41f4219ddc96ae69527670cae1e2b58dc07f8c1f
shunsei <k.shunsei1227@gmail.com>
2025-03-07 22:43:47 +0900

    Define request & response data type

--- frontend/src/types/dream.d.ts
+++ frontend/src/types/dream.d.ts
@@ -1,8 +1,17 @@
-export interface Dream {
-  id?: number;
+export interface RequestDream {
   user_id?: number;
   title: string;
   content: string;
   is_public: boolean;
+}
+
+export interface Dream {
+  id: number;
+  user_id: number;
+  title: string;
+  content: string;
+  is_public: boolean;
   likes: number;
+  created_at: string;
+  updated_at: string;
 }
