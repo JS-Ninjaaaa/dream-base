7c280d6028f204b0447f272e403174c8a48ef8a9
shunsei <k.shunsei1227@gmail.com>
2025-03-08 02:06:05 +0900

    Check if same email user exists before create user

--- backend/models/user.py
+++ backend/models/user.py
@@ -47,7 +47,7 @@ class User:
         return check_password_hash(self.password_hash, password)
 
     @classmethod
-    def create_user(cls, email: str, password: str) -> User:
+    def create_user(cls, email: str, password: str) -> User | None:
         password_hash = generate_password_hash(password)
 
         supabase: Client = get_supabase_client()
