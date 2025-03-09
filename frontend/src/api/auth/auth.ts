14919260b02906fc988a707fdd88e058b72cc150
shunsei <k.shunsei1227@gmail.com>
2025-03-09 20:06:56 +0900

    Add auth login api function

--- frontend/src/api/auth/auth.ts
+++ frontend/src/api/auth/auth.ts
@@ -17,10 +17,32 @@ export const login = async (email: string, password: string) => {
 
   const token = response.headers.get("Authorization");
   if (token === null) {
-    throw new Error("JWTトークンを取得できませんでした");
+    throw new Error("JWTを取得できませんでした");
   }
   sessionStorage.setItem("token", token);
 
   const userInfo: User = await response.json();
   return userInfo;
+};
+
+export const loginWithOAuth = async (userId: string) => {
+  const response = await fetch(`${ENDPOINT}/login/oauth`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userId }),
+  });
+
+  if (!response.ok) {
+    throw new Error("OAuthログインに失敗しました");
+  }
+
+  const token = response.headers.get("Authorization");
+  if (token === null) {
+    throw new Error("JWTを取得できませんでした");
+  }
+  sessionStorage.setItem("token", token);
+
+  return;
 };
