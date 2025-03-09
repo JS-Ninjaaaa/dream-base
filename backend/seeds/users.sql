936589f4c3870baf8491783255772769c30c99f5
cercil0605 <kuamaichi2223@gmail.com>
2025-03-09 15:17:58 +0900

    delete: sql

--- backend/seeds/users.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO
-  users (email, password_hash)
-VALUES
-  ('testuser1@example.com', 'pbkdf2:sha256:260000$kPdtHopNiQrAsNfB$f21ec91948dbc5ed7bde72a07af0aaa9f44e17ba037892b5c9c3f4d480b65703'),
-  ('testuser2@example.com', 'pbkdf2:sha256:260000$kPdtHopNiQrAsNfB$f21ec91948dbc5ed7bde72a07af0aaa9f44e17ba037892b5c9c3f4d480b65703'),
-  ('testuser3@example.com', 'pbkdf2:sha256:260000$kPdtHopNiQrAsNfB$f21ec91948dbc5ed7bde72a07af0aaa9f44e17ba037892b5c9c3f4d480b65703');
