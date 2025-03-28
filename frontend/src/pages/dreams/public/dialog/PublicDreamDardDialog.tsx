7ae4252a1912dba7550830b222fad7e9b0357125
Hikaribamboo <hikari1653@gmail.com>
2025-03-28 19:06:07 +0900

    made publicdialog file

---
+++ frontend/src/pages/dreams/public/dialog/PublicDreamDardDialog.tsx
@@ -0,0 +1,36 @@
+import { Dream } from "@/types/dream";
+import { AlertDialog, Flex } from "@radix-ui/themes";
+
+interface MyDreamCardDialogProps {
+  dream: Dream;
+}
+
+const PublicDreamCardDialog = ({ dream }: MyDreamCardDialogProps) => {
+  return (
+    <AlertDialog.Content className="flex flex-col h-full min-h-[300px] max-w-[400px]">
+      <AlertDialog.Title></AlertDialog.Title>
+      <AlertDialog.Description size="4" className="flex-grow">
+        {dream.content}
+      </AlertDialog.Description>
+      {0 < dream.hashtags.length && (
+        <div className="text-center my-2 text-pink-500">
+          {dream.hashtags.map((tag, i) => (
+            <span key={i} className="mr-3 text-sm font-semibold underline">
+              {"# " + tag.name}
+            </span>
+          ))}
+        </div>
+      )}
+      <Flex gap="3" direction="column" justify="end" align="center">
+        <AlertDialog.Cancel>
+          <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-5 rounded max-w-[100px]">
+            閉じる
+          </button>
+        </AlertDialog.Cancel>
+      </Flex>
+    </AlertDialog.Content>
+  );
+};
+
+export default PublicDreamCardDialog;
+
