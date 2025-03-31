be7e1ac818e9912797e93a829d9ac959a5beef57
Hikaribamboo <hikari1653@gmail.com>
2025-03-31 11:33:54 +0900

    issue #64

--- frontend/src/pages/dreams/mine/card/dialog/MyDreamCardDialog.tsx
+++ frontend/src/pages/dreams/mine/card/dialog/MyDreamCardDialog.tsx
@@ -11,15 +11,15 @@ interface MyDreamCardDialogProps {
 
 const MyDreamCardDialog = ({ dream, setMyDreams, onClose,  }: MyDreamCardDialogProps) => {
   return (
-    <AlertDialog.Content className="flex flex-col h-full min-h-[340px] max-w-[400px]">
+    <AlertDialog.Content className="flex flex-col h-full min-h-[360px] max-w-[400px]">
       <AlertDialog.Title></AlertDialog.Title>
       <AlertDialog.Description size="4" className="flex-grow">
-        {dream.content}
+        <div className="font-mpulus text-2xl text-center">{dream.content}</div>
       </AlertDialog.Description>
       {0 < dream.hashtags.length && (
         <div className="text-center my-2 text-pink-500">
           {dream.hashtags.map((tag, i) => (
-            <span key={i} className="mr-3 text-sm font-semibold underline">
+            <span key={i} className="mr-3 text-lg font-semibold underline">
               {"# " + tag.name}
             </span>
           ))}
