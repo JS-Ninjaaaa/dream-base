08f6d6d173ef002ebdce5f9c2b7ff4afcfe50c6a
Hikaribamboo <hikari1653@gmail.com>
2025-03-31 11:25:09 +0900

    Small UI changes

--- frontend/src/pages/dreams/mine/card/MyDreamCard.tsx
+++ frontend/src/pages/dreams/mine/card/MyDreamCard.tsx
@@ -37,9 +37,9 @@ const MyDreamCard = ({ dream, setMyDreams }: MyDreamCardProps) => {
             >
             <div className="text-base font-mpulus text-center px-6">{dream.content}</div>
             <div className="bg-white rounded-2xl px-4 py-2 flex flex-col items-center">
-              <div className="font-ubuntu flex gap-2 mb-2 items-center">
-                <span className="text-base font-bold">{dream.likes}</span>
-                <p className="text-sm">いいね</p>
+              <div className="font-ubuntu flex gap-2 items-center">
+                <span className="text-lg">{dream.likes}</span>
+                <p className="text-xs">いいね</p>
               </div>
               <MyDreamPrivacyButton dream={dream} setMyDreams={setMyDreams} />
             </div>
