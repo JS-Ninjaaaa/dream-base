ef80128df5378f76660aa8fa931f6b6462192656
shunsei <k.shunsei1227@gmail.com>
2025-04-05 16:27:51 +0900

    Use tailwind default theme break point

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,13 @@
 
+import { defaultTheme } from "tailwindcss/defaultTheme";
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
-        xxs: "320px",
-        xs: "375px",
-        sm: "430px",
-        sms: "540px", 
-        md: "768px",  
-        lg: "1024px",
-        xl: "1280px",
-        xxl: "1536px",     
+        xs: "375px", // iPhone SE
+        ...defaultTheme.screens,
       },
     },
   },
