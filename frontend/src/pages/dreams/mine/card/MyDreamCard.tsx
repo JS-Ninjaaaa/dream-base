fd72fe21cf463305486998c13e028aefe0dc60c3
shunsei <k.shunsei1227@gmail.com>
2025-04-10 00:14:10 +0900

    Limit alert dialog trigger area

--- frontend/src/pages/dreams/mine/card/MyDreamCard.tsx
+++ frontend/src/pages/dreams/mine/card/MyDreamCard.tsx
@@ -8,34 +8,40 @@ interface MyDreamCardProps {
 }
 
 const getPinkGradientClass = (likes: number = 0): string => {
-  if (likes >= 100) return "to-pink-500";
-  if (likes >= 70) return "to-pink-400";
-  if (likes >= 50) return "to-pink-300";
-  if (likes >= 30) return "to-pink-200";
-  if (likes >= 10) return "to-pink-100";
+  if (likes >= 100) {
+    return "to-pink-500";
+  } else if (likes >= 70) {
+    return "to-pink-400";
+  } else if (likes >= 50) {
+    return "to-pink-300";
+  } else if (likes >= 30) {
+    return "to-pink-200";
+  } else if (likes >= 10) {
+    return "to-pink-100";
+  }
   return "to-pink-50";
 };
 
 const MyDreamCard = ({ dream, setMyDreams }: MyDreamCardProps) => {
   const gradientClass = getPinkGradientClass(dream.likes);
   return (
-    <AlertDialog.Trigger>
-      <div className="flex flex-col items-center p-4">
-        {/* 突起 */}
-        <div className="w-[100px] h-[40px] rounded-full bg-[#ffbadf] border-2 border-gray-500 z-10" />
+    <div className="flex flex-col items-center p-4">
+      {/* 突起 */}
+      <div className="w-[100px] h-[40px] rounded-full bg-[#ffbadf] border-2 border-gray-500 z-10" />
 
-        {/* 軸 */}
-        <div className="w-[40px] h-[20px] bg-white border-l-2 border-r-2 border-gray-500 z-0" />
+      {/* 軸 */}
+      <div className="w-[40px] h-[20px] bg-white border-l-2 border-r-2 border-gray-500 z-0" />
 
-        {/* 本体 */}
+      {/* 本体 */}
+      <AlertDialog.Trigger>
         <div
           className={`
-              relative rounded-[50px] md:w-[220px] md:h-[220px] w-[240px] h-[240px] 
-              transform transition-transform duration-300
-              flex flex-col items-center justify-center gap-2
-              bg-gradient-to-b from-white border-2 border-gray-500
-              ${gradientClass} 
-            `}
+            relative rounded-[50px] md:w-[220px] md:h-[220px] w-[240px] h-[240px] 
+            transform transition-transform duration-300
+            flex flex-col items-center justify-center gap-2
+            bg-gradient-to-b from-white border-2 border-gray-500
+            ${gradientClass} 
+          `}
         >
           <div className="text-base font-mpulus text-center px-6">
             {dream.content}
@@ -48,8 +54,8 @@ const MyDreamCard = ({ dream, setMyDreams }: MyDreamCardProps) => {
             <MyDreamPrivacyButton dream={dream} setMyDreams={setMyDreams} />
           </div>
         </div>
-      </div>
-    </AlertDialog.Trigger>
+      </AlertDialog.Trigger>
+    </div>
   );
 };
 
