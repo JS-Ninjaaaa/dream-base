7f7fa83af388aed427063b34778b2f6c11fda38a
shunsei <k.shunsei1227@gmail.com>
2025-04-26 01:15:57 +0900

    Return not only hashtash name but also hashtag id

--- backend/sql/search_public_dream.sql
+++ backend/sql/search_public_dream.sql
@@ -5,31 +5,28 @@ AS $$
 BEGIN
     RETURN QUERY
     SELECT json_build_object(
-        'id', d.id,
+        'id', d.id::text,
         'user_id', d.user_id,
         'content', d.content,
         'is_public', d.is_public,
         'likes', d.likes,
-        'created_at', d.created_at,
-        'updated_at', d.updated_at,
         'hashtags', (
-            SELECT json_agg(h.name ORDER BY h.name)
+            SELECT COALESCE(
+                json_agg(
+                    json_build_object(
+                        'id', h.id::text,
+                        'name', h.name
+                    ) ORDER BY h.name
+                ),
+                '[]'::json
+            )
             FROM dream_hashtags dh
             JOIN hashtags h
                 ON dh.hashtag_id = h.id
             WHERE dh.dream_id = d.id
         ),
-        -- pg_groonga のスコアを取得
-        'score', (
-            pgroonga_score(d.tableoid, d.ctid)
-            + COALESCE((
-                SELECT max(pgroonga_score(h.tableoid, h.ctid))
-                FROM dream_hashtags dh
-                JOIN hashtags h
-                    ON dh.hashtag_id = h.id
-                WHERE dh.dream_id = d.id AND h.name &@~ keyword -- ハッシュタグ名で検索
-            ), 0.0)
-        )
+        'created_at', d.created_at,
+        'updated_at', d.updated_at
     )
     FROM dreams d
     WHERE
@@ -45,7 +42,7 @@ BEGIN
                 WHERE dh.dream_id = d.id AND h.name &@~ keyword -- ハッシュタグ名で検索
             )
         )
-    -- スコアの高い順に並び替え
+    -- 関連度の高い順に並び替え
     ORDER BY (
         pgroonga_score(d.tableoid, d.ctid)
         + COALESCE((
