4024022a74d66a9026ec81643be34e65d4200a85
shunsei <k.shunsei1227@gmail.com>
2025-04-26 00:54:34 +0900

    Add public dream search rpc function

---
+++ backend/sql/search_public_dream.sql
@@ -0,0 +1,60 @@
+CREATE OR REPLACE FUNCTION search_public_dreams(keyword TEXT)
+RETURNS TABLE (hit_dream json)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT json_build_object(
+        'id', d.id,
+        'user_id', d.user_id,
+        'content', d.content,
+        'is_public', d.is_public,
+        'likes', d.likes,
+        'created_at', d.created_at,
+        'updated_at', d.updated_at,
+        'hashtags', (
+            SELECT json_agg(h.name ORDER BY h.name)
+            FROM dream_hashtags dh
+            JOIN hashtags h
+                ON dh.hashtag_id = h.id
+            WHERE dh.dream_id = d.id
+        ),
+        -- pg_groonga のスコアを取得
+        'score', (
+            pgroonga_score(d.tableoid, d.ctid)
+            + COALESCE((
+                SELECT max(pgroonga_score(h.tableoid, h.ctid))
+                FROM dream_hashtags dh
+                JOIN hashtags h
+                    ON dh.hashtag_id = h.id
+                WHERE dh.dream_id = d.id AND h.name &@~ keyword -- ハッシュタグ名で検索
+            ), 0.0)
+        )
+    )
+    FROM dreams d
+    WHERE
+        d.is_public = TRUE
+        AND (
+            d.content &@~ keyword -- 夢の内容で検索
+            OR
+            EXISTS ( -- 関連するハッシュタグで検索
+                SELECT 1
+                FROM dream_hashtags dh
+                JOIN hashtags h
+                    ON dh.hashtag_id = h.id
+                WHERE dh.dream_id = d.id AND h.name &@~ keyword -- ハッシュタグ名で検索
+            )
+        )
+    -- スコアの高い順に並び替え
+    ORDER BY (
+        pgroonga_score(d.tableoid, d.ctid)
+        + COALESCE((
+            SELECT max(pgroonga_score(h.tableoid, h.ctid))
+            FROM dream_hashtags dh
+            JOIN hashtags h
+                ON dh.hashtag_id = h.id
+            WHERE dh.dream_id = d.id AND h.name &@~ keyword
+        ), 0.0)
+    ) DESC;
+END;
+$$;
