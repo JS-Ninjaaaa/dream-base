ffe8bee5c43da65c53babc8c386a6fc9903bf0c2
shunsei <k.shunsei1227@gmail.com>
2025-05-05 21:38:38 +0900

    Move sakura images

--- frontend/src/pages/dreams/components/SakuraScatterEffect.tsx
+++ frontend/src/pages/dreams/components/SakuraScatterEffect.tsx
@@ -1,14 +1,14 @@
 const SakuraScatterEffect = () => {
   const sakuraImagePaths = Array(3)
     .fill([
-      "/assets/s1.png",
-      "/assets/s2.png",
-      "/assets/s3.png",
-      "/assets/s4.png",
-      "/assets/s5.png",
-      "/assets/s6.png",
-      "/assets/s7.png",
-      "/assets/s8.png",
+      "/assets/images/s1.png",
+      "/assets/images/s2.png",
+      "/assets/images/s3.png",
+      "/assets/images/s4.png",
+      "/assets/images/s5.png",
+      "/assets/images/s6.png",
+      "/assets/images/s7.png",
+      "/assets/images/s8.png",
     ])
     .flat();
 
